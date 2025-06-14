7c3351e0cde882bc866275b1e5f819ba88f06b35
SamInfine8 <sameer8creative.tech@gmail.com>
2025-06-14 12:03:32 +0530

    Day-4 with Express and EJS

--- EJS/index.js
+++ EJS/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 8080;
+
+
+//views dirctory
+app.set("views", path.join(__dirname,"/views"));
 
 app.set("view engine", "ejs");
 
@@ -8,10 +13,23 @@ app.get("/", (req, res)=>{
     res.render("home.ejs");
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
-
 app.get("/project", (req, res)=>{
     res.render("projects.ejs");
+});
+
+
+app.get("/ig/:username/:follow", (req, res)=>{
+    let {username, follow} = req.params;
+    res.render("ig.ejs",{username:username , follow:follow});
+});
+
+//passing data to EJS
+app.get("/dice", (req, res)=>{
+    let diceValue = Math.floor(Math.random() *6)+1;
+    res.render("Dice.ejs", {val : diceValue});
 })
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
