6e5da15cd751af3204d94fd52560f5eeeb72b0d7
SamInfine8 <sameer8creative.tech@gmail.com>
2025-06-17 12:24:26 +0530

    Day-7 Starting with RESTful APIs

---
+++ RESTful/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+
+let posts = [
+    {
+        user:"sameer",
+        content: "Hello , i a stating REST",
+        likes: 0,
+    },
+    {
+        user:"appu",
+        content: "I am developing something CRAZY",
+        likes:23,
+    },
+    {
+        user:"techie",
+        content: "Tech Is Growing AT Faster Rate and Here is HOW",
+        likes: 99,
+    }
+]
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); 
+
+
+app.get("/",(req, res)=>{
+    res.render("home.ejs");
+});
+
+app.get("/posts",(req, res)=>{
+    res.render("posts.ejs", {posts});
+});
+
+app.listen(port , ()=>{
+    console.log(`Server is running on port:${port}`);
+})
