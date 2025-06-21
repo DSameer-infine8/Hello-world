17c12b86d2157142940c1004cb7ba724386533cb
SamInfine8 <sameer8creative.tech@gmail.com>
2025-06-21 13:48:01 +0530

    Day-11 with Node+SQL(Frontend,Backend and DB:Integrated)

--- nodeWithSQL/index.js
+++ nodeWithSQL/index.js
@@ -2,7 +2,19 @@ const express = require("express");
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
 const app = express();
+const path = require("path");
+const methodOverride = require('method-override');
+
+
 const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 
 const connection = mysql.createConnection({
@@ -12,15 +24,41 @@ const connection = mysql.createConnection({
   password: 'Sam@SQL8'
 });
 
-let q ="SHOW TABLEs"
+const getRandomUsers = () => {
+  let d= [
+    faker.string.uuid(),
+    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(), 
+    faker.internet.password()
+  ]
+  return d
+}
+
+
+
+//Inserting New DATAs
+/*
+let q ="insert into user (id , name , email , password) values ?";
+let data = ["123@abc", "abcRandom" , "abcrandom@gmail.com", "abc@123"];
+let datas = [["123@abc2", "abcRandom2" , "abcrandom@gmail.com2", "abc@123b"],
+             ["123@abc3", "abcRandom3" , "abcrandom@gmail.com3", "abc@123c"]]
+
+
+//Insert in  Bulk(Using Faker)
+let q = "Insert into user(id , name , email, password) values ?";
+let data = [];
+for (let i = 1; i<= 100; i++){
+  data.push(getRandomUsers());
+}
+
+console.log(data);
+
+
 
 try {
-    connection.query(q , (err, result)=>{
+    connection.query(q ,[data], (err, result)=>{
     if (err) throw err;
     console.log(result);
-    console.log(result.length);
-    console.log(result[0]);
-    console.log(result[1].Tables_in_app);
 });
 } catch (error) {
     console.log(err);
@@ -34,22 +72,89 @@ Server is running on port 8080
 { Tables_in_app: 'temp' }
 user
 
-*/
 
 connection.end();
- 
-const getRandomUsers = () => {
-  return {
-    Id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(), 
-    password: faker.internet.password()
-  };
-}
+ */
+
+app.get("/", (req, res)=>{
+  let q=`select count(*) from user`;
+  try{
+    connection.query(q, (err, result)=>{
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", {count});
+      });
+    }catch(err){
+      console.log(err);
+    }
+});
+
+app.get("/user", (req,res)=>{
+  let q=`SELECT * FROM USER`
+  try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err;
+      let results = result;
+      res.render("user.ejs",{results});
+    })
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/user",(req,res)=>{
+  let q=`SELECT * FROM USER`
+  try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err;
+      let results = result;
+      res.render("user.ejs",{results});
+    })
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//EDIT Route
+
+app.get("/user/:id/edit", (req,res)=>{
+  let id = req.params.id;
+  let q= `SELECT * FROM USER WHERE id='${id}'`
+  try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err;
+      let userDetail = result[0];
+      res.render("edit.ejs",{userDetail});
+    })
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 
-console.log(getRandomUsers());
-
+//Update Route
+app.patch("/user/:id" , (req, res)=>{
+  let id = req.params.id;
+  let q= `SELECT * FROM USER WHERE id='${id}'`
+  let {password, name}= req.body;
+  try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err;
+      let userDetail = result[0];
+      if(password != userDetail.password){
+        res.status(400).send("Password is incorrect");
+      }else{
+        let q2 = `UPDATE USER SET name='${name}' WHERE id='${id}'`
+        connection.query(q2, (err, result)=>{
+          if(err) throw err;
+          res.redirect("/user");
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
--- nodeWithSQL/schema.sql
+++ nodeWithSQL/schema.sql
@@ -1,1 +1,6 @@
-SHOW TABLES;
+create table user(
+    id varchar(50) primary key,
+    name varchar(50) unique,
+    email varchar(50) unique not null,
+    password varchar(50) unique not null
+);
