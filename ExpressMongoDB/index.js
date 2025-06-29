e839e5666feb27a1f9f10e402f4090af493949a9
SamInfine8 <sameer8creative.tech@gmail.com>
2025-06-29 13:47:26 +0530

    Day-13 with Express+MongoDB(GET & POST for ChatApp)

--- ExpressMongoDB/index.js
+++ ExpressMongoDB/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path =  require("path");
 const mongoose = require("mongoose");
-const chat = require("./models/chat.js");
+const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
 const port = 8080;
 
@@ -18,6 +18,33 @@ const main = async() =>{
 
 main().then(()=>{console.log("connection successful")})
 .catch((err)=>{console.log(err)});
+
+app.get("/chat",async (req,res)=>{
+    let chats = await Chat.find();
+    res.render("index.ejs", {chats})
+});
+
+app.get("/",(req,res)=>{
+    console.log(chat.find());
+    res.status(200).send("Welcome to chat app!");
+});
+
+app.post("/chat",(req, res)=>{
+    let {from , to , msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        create_at: new Date()
+
+    });
+    newChat.save().then((res)=>{
+        console.log("chat saved");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chat");
+})
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
